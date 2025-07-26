310715e47a0bff5201926abdec75b39bfb63302f
Tanisha-Sharma005 <tanishaasharma20@gmail.com>
2025-07-26 21:42:56 +0530

    Update factorial_function.c

--- Function Program/factorial_function.c
+++ Function Program/factorial_function.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 int factorial(int n) {
-    if (n == 0) 
-      return 1;
-    else
-      return n * factorial(n - 1);
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
 }
 
 int main() {
     int num;
+
     printf("Enter a number: ");
     scanf("%d", &num);
-    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    if(num < 0)
+        printf("Factorial not defined for negative numbers.\n");
+    else
+        printf("Factorial of %d = %d\n", num, factorial(num));
+
     return 0;
 }
