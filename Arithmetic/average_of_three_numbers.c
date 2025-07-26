375c1f4a72b971ad87e5ae672410d6722e34393a
Tanisha-Sharma005 <tanishaasharma20@gmail.com>
2025-07-26 20:14:36 +0530

    Create average_of_three_numbers.c

---
+++ Arithmetic/average_of_three_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a , b ;
+  printf("Enter the two numbers");
+  scanf("%d%d",&a,&b);
+    if (a > b)
+        printf("Max = %d\n", a);
+    else
+        printf("Max = %d\n", b);
+    return 0;
+}
