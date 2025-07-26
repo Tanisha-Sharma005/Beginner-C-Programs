6c919310ed62f8ff6e29ed8d7c7e39019e1c2c9c
Tanisha-Sharma005 <tanishaasharma20@gmail.com>
2025-07-26 20:33:33 +0530

    Create max_in_array.c

---
+++ Array Programs/max_in_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a[100], n, i, max;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    max = a[0];
+    for(i = 1; i < n; i++)
+        if(a[i] > max)
+            max = a[i];
+    printf("Maximum element = %d", max);
+    return 0;
+}
