5eac08e2def2b49351ce87baec61d2e13116c90b
Tanisha <tanishaasharma20@gmail.com>
2025-07-28 17:51:53 +0530

    Create prime_number_check.c

---
+++ Miscellaneous_Programs/prime_number_check.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num = 29, isPrime = 1;
+
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
